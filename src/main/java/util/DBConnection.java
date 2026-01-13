260967a35c800075d1307ef274550013c6128a2d
kavinda1996 <gayashankavinda95@gmail.com>
2026-01-13 11:18:31 +0530

    create DBConnection class

---
+++ src/main/java/util/DBConnection.java
@@ -0,0 +1,37 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/user_authentication_db?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+
+    private DBConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found", e);
+        }
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        if (dbConnection == null) {
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
+
